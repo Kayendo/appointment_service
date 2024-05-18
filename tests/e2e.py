6e7bde83a43f4dfcaada18691406d27997396405
Kayendo <vlad.sh.123@outlook.com>
2024-05-18 20:22:45 +0300

    test

---
+++ tests/e2e.py
@@ -0,0 +1,15 @@
+import pytest
+import requests
+
+from datetime import datetime
+
+from app.models.appointment import Appoitment
+
+time.sleep(5) 
+base_url = 'http://localhost:8000/api'
+@pytest.fixture(scope='session') 
+def first_delivery_data() -> tuple[UUID, str, datetime]: 
+	return (uuid4(), 'address_1', datetime.now())
+
+def test_get_appoinments() -> None: 
+	assert requests.get(f'{base_url}/appointments').json() == [Appointment(appointment_id=1, cabinet = 2, doctors_name='Steve')]
